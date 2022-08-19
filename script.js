68fde0d80dfe5a5882fea0dea4dc0da25835f0ed
Gabriel Gusso <gabriel.gusso@hotmail.com>
2022-08-19 04:07:45 -0300

    Flip Cards

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ const numberOfCards = prompt(
 );
 const list = document.querySelector(".cards");
 
-let i = 0;
-
-while (
+for (
+  let i = 0;
   numberOfCards > i &&
   numberOfCards % 2 === 0 &&
   numberOfCards >= 4 &&
-  numberOfCards <= 14
+  numberOfCards <= 14;
+  i++
 ) {
   list.innerHTML =
     list.innerHTML +
-    `<li class="card"><img src="img/front.png" alt="parrot-card"></li>`;
-  i++;
+    `<li class="card" onclick="flip(this)">
+    <div class="face first">
+      <img src="img/front.png" alt="front-card" />
+    </div>
+    <div class="face back-face-default">
+      <img src="img/fiestaparrot.gif" alt="back-card" />
+    </div>
+  </li>`;
+}
+
+function flip(element) {
+  const child1 = element.children[0];
+  const child2 = element.children[1];
+  child1.classList.toggle("front-face");
+  child2.classList.toggle("back-face");
 }
